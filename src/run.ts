41447f7db054444a36bab198b2aae21fabbc0862
Hidetake Iwata <int128@gmail.com>
2023-10-09 12:22:00 +0900

    Add graphql-codegen

--- src/run.ts
+++ src/run.ts
@@ -1,10 +1,35 @@
+import assert from 'assert'
 import * as core from '@actions/core'
+import * as github from '@actions/github'
+import { getAssociatedPullRequest } from './queries/getAssociatedPullRequest'
 
 type Inputs = {
-  name: string
+  sha: string
+  token: string
 }
 
-// eslint-disable-next-line @typescript-eslint/require-await
 export const run = async (inputs: Inputs): Promise<void> => {
-  core.info(`my name is ${inputs.name}`)
+  const octokit = github.getOctokit(inputs.token)
+
+  core.info(`Getting the associated pull request for the commit ${inputs.sha}`)
+  const associatedPullRequest = await getAssociatedPullRequest(octokit, {
+    owner: github.context.repo.owner,
+    name: github.context.repo.repo,
+    expression: inputs.sha,
+  })
+
+  assert(associatedPullRequest.repository)
+  assert(associatedPullRequest.repository.object)
+  assert.strictEqual(associatedPullRequest.repository.object.__typename, 'Commit')
+  assert(associatedPullRequest.repository.object.associatedPullRequests)
+  assert(associatedPullRequest.repository.object.associatedPullRequests.nodes != null)
+
+  if (associatedPullRequest.repository.object.associatedPullRequests.nodes.length === 0) {
+    core.info('No associated pull request found')
+    return
+  }
+  for (const node of associatedPullRequest.repository.object.associatedPullRequests.nodes) {
+    assert(node != null)
+    core.info(`Found associated pull request #${node.number}`)
+  }
 }
